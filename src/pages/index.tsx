459955eb600c89c1197ff44febd8fac347f134d4
Igor Santos <igorsantoscosta@gmail.com>
2022-12-16 15:24:29 -0300

    Add responsive css #3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <>
       <PaymentCardProvider>
-        <Box className="mt-48 flex flex-col gap-10 max-w-[90%] md:max-w-3xl">
+        <Box className="mt-36 md:mt-48 flex flex-col gap-10 max-w-[90%] md:max-w-3xl">
           <Card showPaymentCard={showPaymentCard} className="-mt-32" />
           <FormPayment setShowPaymentCard={setShowPaymentCard} />
         </Box>
